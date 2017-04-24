17df3b53e858a759feaecefd4cda8600111617e0
Daniel Lopez and Justin Gomez <justin@>
2017-04-24 14:57:25 -0700

    coded more effienctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@ var convertGallons = function() {
   return gallons * 3.79;
 }
 
-var resultGallons = convertGallons();
 
-alert('You have ' + resultGallons + ' liters.');
+alert('You have ' + convertGallons() + ' liters.');
 
 var convertLiters = function() {
 	var liters = parseFloat(prompt("How many Liters do you have?"));
   return liters / 3.79;
 }
 
-var resultLiters = convertLiters();
 
-alert("You have " + resultLiters + " gallons");
+alert("You have " + resultLiters() + " gallons");
