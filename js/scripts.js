c9306d3eb65037b12f307abf51a5f41bca78e7cd
Daniel Lopez and Justin Gomez <justin@>
2017-04-24 14:45:58 -0700

    Convert liters to gallons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-var convert = function () {
+var convertGallons = function() {
 	var gallons = parseFloat(prompt("How many gallons do you have?"));
-  return gallons * 3.79 ;
+  return gallons * 3.79;
 }
 
-var result = convert();
+var resultGallons = convertGallons();
 
-alert('You have ' + result + ' liters.');
+alert('You have ' + resultGallons + ' liters.');
+
+var convertLiters = function() {
+	var liters = parseFloat(prompt("How many Liters do you have?"));
+  return liters / 3.79;
+}
+
+var resultLiters = convertLiters();
+
+alert("You have " + resultLiters + " gallons");
